cf7bd1cdc1ebbba55cb1a6ab6b13b202dd573b4c
Artem Lyksa <artemlyksa@gmail.com>
2016-04-10 14:50:58 +0300

    1

--- Challenge Starter/NSUserDefaultsChallengeStarter/ViewController.swift
+++ Challenge Starter/NSUserDefaultsChallengeStarter/ViewController.swift
@@ -10,10 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        var totalOpenings = userDefaults.integerForKey("openings")
+        totalOpenings += 1
+        userDefaults.setInteger(totalOpenings, forKey: "openings")
+        let alreadyRated = userDefaults.boolForKey("AlreadyRated")
+        
+        if !alreadyRated && totalOpenings >= 3 {
+            let alertController = UIAlertController(title: "Rate Our App", message: "If you are enjoyed our app please make sure to rate it", preferredStyle: .Alert)
+            let rateApp = UIAlertAction(title: "RateApp", style: .Default, handler: { (UIAlertAction) in
+                userDefaults.setBool(true, forKey: "AlreadyRated")
+                self.dismissViewControllerAnimated(true, completion: nil)
+            })
+            let close = UIAlertAction(title: "CloseApp", style: .Default, handler: { (UIAlertAction) in
+                self.dismissViewControllerAnimated(true, completion: nil)
+            })
+            alertController.addAction(rateApp)
+            alertController.addAction(close)
+            presentViewController(alertController, animated: true, completion: nil)
+            
+        }
+    }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
